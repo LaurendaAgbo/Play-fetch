ac2d25963e55b84ef17fb2b853532ee0fe32f6a5
LaurendaA <alaurenda2@gmail.com>
2024-09-25 03:53:40 +0100

    Only allow the player to spawn a new dog after a certain amount of time has passed

--- Assets/Challenge 2/Scripts/PlayerControllerX.cs
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -5,14 +5,19 @@ using UnityEngine;
 public class PlayerControllerX : MonoBehaviour
 {
     public GameObject dogPrefab;
+    private float lastFired;
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
         // On spacebar press, send dog
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            if ((lastFired - Time.time) < -1)
+            {
+                Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+                lastFired = Time.time;
+            }
         }
     }
 }
